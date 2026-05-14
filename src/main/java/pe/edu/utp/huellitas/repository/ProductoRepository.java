305f8352d7dabc8ce326460807af031f55d99b35
Tatiana Tello <bktatiana0@gmail.com>
2026-05-13 19:57:15 -0500

    correcciones menores,  faltan

--- src/main/java/pe/edu/utp/huellitas/repository/ProductoRepository.java
+++ src/main/java/pe/edu/utp/huellitas/repository/ProductoRepository.java
@@ -1,10 +1,13 @@
 package pe.edu.utp.huellitas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pe.edu.utp.huellitas.model.Producto;
 import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    //método de conteo para el dashboard
-    List<Producto> findByStockActualLessThanEqualStockMinimo();
+
+    //@Query para el coptrol de la lógica de comparación de columnas
+    @Query("SELECT p FROM Producto p WHERE p.stockActual <= p.stockMinimo")
+    List<Producto> findProductosConStockCritico();
 }
