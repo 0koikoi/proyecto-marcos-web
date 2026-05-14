043eae332075fa4e0d079b4476b39c9d518f5ea1
Xcap7inOwl <174526778+Xcap7inOwl@users.noreply.github.com>
2026-05-14 13:36:14 -0500

    changes 02 fix

---
+++ src/main/java/pe/edu/utp/huellitas/web/DashboardController.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.huellitas.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+}
