7ba0fe4aedf4f2693cddd3dab213c3c44d3711ff
Tatiana Tello <bktatiana0@gmail.com>
2026-06-09 06:51:53 -0500

    correcciones de vistas, falta más manden ayuda

--- src/main/java/pe/edu/utp/huellitas/controller/WebController.java
+++ src/main/java/pe/edu/utp/huellitas/controller/WebController.java
@@ -9,7 +9,6 @@ import pe.edu.utp.huellitas.repository.ProductoRepository;
 
 @Controller
 public class WebController {
-
     private final PacienteRepository pacienteRepo;
     private final CitaRepository citaRepo;
     private final ProductoRepository productoRepo;
@@ -22,33 +21,19 @@ public class WebController {
 
     @GetMapping("/dashboard")
     public String verDashboard(Model model) {
-        //manda de la bd, no datos estáticos
         model.addAttribute("totalPacientes", pacienteRepo.count());
-        model.addAttribute("citasHoy", citaRepo.count()); // Aquí podrías filtrar por fecha
-        model.addAttribute("stockCritico", productoRepo.findProductosConStockCritico().size());        return "dashboard";
+        model.addAttribute("citasHoy", citaRepo.count());
+        model.addAttribute("stockCritico", productoRepo.findProductosConStockCritico().size());
+        return "dashboard";
     }
 
-    @GetMapping("/inventario")
-    public String verInventario() { return "inventario"; }
-
-    @GetMapping("/reportes")
-    public String verReportes() { return "reportes"; }
-
-    @GetMapping("/configuracion")
-    public String verConfiguracion() { return "configuracion"; }
-
-    @GetMapping("/ayuda")
-    public String verAyuda() { return "ayuda"; }
-
-    @GetMapping("/") //sim
+    @GetMapping("/")
     public String mostrarLogin() {
         return "login";
     }
 
-    @GetMapping("/logout") //parche temporal
+    @GetMapping("/logout")
     public String simularLogout() {
-        //nos faltó login profe
         return "redirect:/";
     }
 }
-
